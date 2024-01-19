f51e319004fb69aca1fc9b60f32c59148b545d40
FuadXyro <fuadxy99@gmail.com>
2024-01-19 15:02:35 +0100

    ZenithBotz

--- plugins/afk_ans.js
+++ plugins/afk_ans.js
@@ -1,71 +1,32 @@
 export async function before(m) {
     let senderUser = global.db.data.users[m.sender]
 
-    if (senderUser.afk > -1) {
-        m.reply(`
-  Kamu berhenti AFK${senderUser.afkReason ? ' setelah ' + senderUser.afkReason : ''}
-  Selama ${(new Date() - senderUser.afk).toTimeString()}
-  `.trim())
+    if (senderUser.afk > -1) m.reply(`Kamu berhenti AFK${senderUser.afkReason ? ' setelah ' + senderUser.afkReason : ''} Selama ${(new Date() - senderUser.afk).toTimeString()}`.trim()), senderUser.afk = -1, senderUser.afkReason = ''
 
-        senderUser.afk = -1
-        senderUser.afkReason = ''
-    }
-     
-    const textLower = m.text.toLowerCase()
-    const rp = new Intl.NumberFormat('id-ID', {
-        style: 'currency',
-        currency: 'IDR',
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 0,
-    })
-
+    const textLower = m.text.toLowerCase(), rp = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0, maximumFractionDigits: 0 })
     let mentionedJids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
 
-    for (let jid of mentionedJids) {
-        let taggedUser = global.db.data.users[jid]
+    if (/^maaf$/i.test(textLower)) global.db.data.users[m.sender].afk = -1, m.reply('Anda telah meminta maaf dan terhindar dari denda.')
 
-        if (!taggedUser) {
-            continue
-        }
+    if (mentionedJids.length > 0) {
+        this.dendaMessageSent = false;
+        for (let jid of mentionedJids) {
+            let taggedUser = global.db.data.users[jid]
 
-        let afkTime = taggedUser.afk
+            if (!taggedUser || taggedUser.afk < 0) continue;
 
-        if (!afkTime || afkTime < 0) {
-            continue
-        }
+            let afkTime = taggedUser.afk, reason = taggedUser.afkReason || '', maxDenda = 100000000, denda = Math.floor(Math.random() * maxDenda) + 1;
 
-        let reason = taggedUser.afkReason || ''
-        let maxDenda = 100000000
-        let denda = Math.floor(Math.random() * maxDenda) + 1
+            if (!this.dendaMessageSent) {
+                let pesan = `Anda akan terkena denda ${rp.format(denda)} Karena mengetag user yang sedang AFK\n${reason ? 'Dengan alasan ' + reason : 'Tanpa alasan'} Selama ${(new Date() - afkTime).toTimeString()}\n• Ketik *maaf* Agar Terhindar dari denda\n• Waktu meminta maaf: 90 Detik`
+                await m.reply(pesan), dendaMessageSent = true;
+            }
 
-        let pesan = `
-Anda akan terkena denda ${rp.format(denda)}
-Karena mengetag user yang sedang AFK
-  
-${reason ? 'Dengan alasan ' + reason : 'Tanpa alasan'}
-Selama ${(new Date() - afkTime).toTimeString()}
+            await sleep(90000)
 
-• Ketik *maaf* Agar Terhindar dari denda
-• Waktu meminta maaf: 90 Detik
-`
-
-        await m.reply(pesan)
-
-        await sleep(90000)
-
-        
-        if (/^maaf$/i.test(textLower)) {
-            global.db.data.users[m.sender].afk = -1
-            return m.reply('Anda telah meminta maaf dan terhindar dari denda.')
-        } else {
-            return m.reply('Anda tidak meminta maaf, maka anda terkena denda')
-            senderUser.money = (senderUser.money || 0) - denda
+            m.reply('Anda tidak meminta maaf, maka anda terkena denda'), senderUser.money = (senderUser.money || 0) - denda, 1;
         }
     }
-
-   // return true
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
+function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
