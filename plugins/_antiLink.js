0e8e29d91b986401d0af04aadf760f3a1d6c4d77
FuadXyro <fuadxy99@gmail.com>
2024-01-21 04:10:25 +0100

    ZenithBotz

--- plugins/_antiLink.js
+++ plugins/_antiLink.js
@@ -6,25 +6,33 @@ export async function before(m, { isAdmin, isBotAdmin }) {
     }
 
     const chat = global.db.data.chats[m.chat]
-    const isGroupLink = linkRegex.exec(m.text)
+    const isGroupLink = linkRegex.test(m.text)
     let kickMessage = ''
-
-    if (isAdmin) {
-        kickMessage = `( ! ) *Tautan Terdeteksi*\nAnda admin grup tidak bisa dikeluarkan dari grup.`
+    
+    if (chat.linkCounter && chat.linkCounter[m.sender] >= 3) {
+        kickMessage = `( ! ) *Tautan Terdeteksi*\nJika Anda mengirim lebih dari 3 kali, Anda akan dikeluarkan dari grup.`
     } else {
-        kickMessage = `( ! ) *Tautan Terdeteksi*\nAnda akan dikeluarkan dari grup.`
-    }
+        kickMessage = isAdmin
+            ? `( ! ) *Tautan Terdeteksi*\nAnda admin grup, tidak bisa dikeluarkan dari grup.`
+            : `( ! ) *Tautan Terdeteksi*\nAnda akan dikeluarkan dari grup.`
 
-    if (chat.antiLink && isGroupLink) {
-        await this.reply(m.chat, kickMessage, fakes, { mentions: [m.sender] })
-        await this.sendMessage(m.chat, { delete: m.key })
+        if (chat.antiLink && isGroupLink) {
+            chat.linkCounter[m.sender] = (chat.linkCounter[m.sender] || 0) + 1
 
-        if ((!isBotAdmin && isAdmin) || (isBotAdmin && !isAdmin)) {
-            await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-            await this.reply(m.chat, kickMessage, fkontak, { mentions: [m.sender] })
-            await this.sendMessage(m.chat, { delete: m.key })
+            await handleKickAndReply.call(this, m, isAdmin, isBotAdmin, kickMessage)
         }
     }
 
     return true
+}
+
+async function handleKickAndReply(m, isAdmin, isBotAdmin, kickMessage) {
+    await this.reply(m.chat, kickMessage, fakes, { mentions: [m.sender] })
+    await this.sendMessage(m.chat, { delete: m.key })
+
+    if ((!isBotAdmin && isAdmin) || (isBotAdmin && !isAdmin)) {
+        await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+        await this.reply(m.chat, kickMessage, fkontak, { mentions: [m.sender] })
+        await this.sendMessage(m.chat, { delete: m.key })
+    }
 }
--- plugins/ro-pairing.js
+++ plugins/ro-pairing.js
@@ -1,9 +1,9 @@
-import SiBail from "@adiwajshing/baileys";
-import Pino from "pino";
-import NodeCache from "node-cache";
-import WebSocket from 'ws';
-import path from "path";
-import fs from 'fs';
+import SiBail from "@adiwajshing/baileys"
+import Pino from "pino"
+import NodeCache from "node-cache"
+import WebSocket from 'ws'
+import path from "path"
+import fs from 'fs'
 
 const {
   useMultiFileAuthState,
@@ -12,10 +12,10 @@ const {
   jidNormalizedUser,
   makeCacheableSignalKeyStore,
   PHONENUMBER_MCC
-} = SiBail;
+} = SiBail
 
-const store = makeInMemoryStore({ logger: Pino({ level: "fatal" }).child({ level: "fatal" }) });
-global.conns = [];
+const store = makeInMemoryStore({ logger: Pino({ level: "fatal" }).child({ level: "fatal" }) })
+global.conns = []
 
 let handler = async (m, {
   conn,
@@ -24,11 +24,15 @@ let handler = async (m, {
   command,
   isOwner
 }) => {
-  process.on("unhandledRejection", (err) => console.error(err));
+  process.on("unhandledRejection", (err) => console.error(err))
 
-  const numJadibot = args[0];
-  const { state, saveCreds } = await useMultiFileAuthState('./jadibot/' + numJadibot + '.json');
-  const msgRetryCounterCache = new NodeCache();
+  // Check if the number argument is provided
+  if (!args[0]) return m.reply('Input Number.')
+
+  const numJadibot = args[0]
+  const { state, saveCreds } = await useMultiFileAuthState('./jadibot/' + numJadibot + '.json')
+  const msgRetryCounterCache = new NodeCache()
+  // Use 'const' instead of 'let' for better code readability
   const bajing = SiBail.default({
     logger: Pino({ level: "fatal" }).child({ level: "fatal" }),
     printQRInTerminal: false,
@@ -41,112 +45,113 @@ let handler = async (m, {
     markOnlineOnConnect: true,
     generateHighQualityLinkPreview: true,
     getMessage: async (key) => {
-      let jid = jidNormalizedUser(key.remoteJid);
-      let msg = await store.loadMessage(jid, key.id);
+      let jid = jidNormalizedUser(key.remoteJid)
+      let msg = await store.loadMessage(jid, key.id)
 
-      return msg?.message || "";
+      return msg?.message || ""
     },
     msgRetryCounterCache,
     defaultQueryTimeoutMs: undefined,
-  });
-  store.bind(bajing.ev);
-  let ev = bajing.ev;
+  })
+  store.bind(bajing.ev)
+  let ev = bajing.ev
 
-  if (conn.user.jid == numJadibot) return m.reply('Tidak bisa membuat Bot pada user jadibot!');
+  if (conn.user.jid == numJadibot) return m.reply('Cannot create a bot for the jadibot user!')
 
-  let authFile = './jadibot/' + numJadibot + '.json';
-  let isInit = !fs.existsSync(authFile);
-  let id = global.conns.length;
+  let authFile = './jadibot/' + numJadibot + '.json'
+  let isInit = !fs.existsSync(authFile)
+  let id = global.conns.length
 
-  let date = new Date();
-  let timestamp = date.getHours() + ':' + date.getMinutes() + ' ' + date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
-  bajing.timestamp = timestamp;
+  let date = new Date()
+  let timestamp = date.getHours() + ':' + date.getMinutes() + ' ' + date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear()
+  bajing.timestamp = timestamp
 
+  
   async function needUpdate(update) {
     const {
       connection,
       lastDisconnect
-    } = update;
-    date = new Date();
-    console.log(update);
-    timestamp = date.getHours() + ':' + date.getMinutes() + ' ' + date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
-    bajing.timestamp = timestamp;
+    } = update
+    date = new Date()
+    console.log(update)
+    timestamp = date.getHours() + ':' + date.getMinutes() + ' ' + date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear()
+    bajing.timestamp = timestamp
 
     if (!bajing.authState.creds.registered) {
-      if (!isNaN(global.tryConnect[numJadibot])) global.tryConnect[numJadibot] = 0;
+      if (!isNaN(global.tryConnect[numJadibot])) global.tryConnect[numJadibot] = 0
       if (global.tryConnect[numJadibot] === 3) {
-        global.tryConnect[numJadibot] = 0;
-        return m.reply('Waktu scan qr kamu sudah habis!');
+        global.tryConnect[numJadibot] = 0
+        return m.reply('Waktu scan qr kamu sudah habis!')
       }
 
-      let code = await bajing.requestPairingCode(numJadibot);
-      code = code?.match(/.{1,4}/g)?.join("-") || code;
-      console.log(code);
+      let code = await bajing.requestPairingCode(numJadibot)
+      code = code?.match(/.{1,4}/g)?.join("-") || code
+      console.log(code)
 
-      let scan = await m.reply(code);
+      let scan = await m.reply(code)
       setTimeout(() => {
         bajing.sendMessage(m.chat, {
           delete: scan.key
-        });
-      }, 30000);
-      global.tryConnect[numJadibot] += 1;
+        })
+      }, 30000)
+      global.tryConnect[numJadibot] += 1
     }
 
     if (lastDisconnect && lastDisconnect.error && lastDisconnect.error.output && lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut && bajing.ws.readyState !== WebSocket.CONNECTING) {
-      global.tryConnect(true);
-      m.reply('Connecting...');
+      global.tryConnect(true)
+      m.reply('Connecting...')
     } else if (connection === 'open') {
-      global.conns.push(bajing);
-      bajing.reply(m.chat, `Berhasil Tersambung dengan WhatsApp mu.\n*NOTE: Cuma Numpang!*\nNomor: ${bajing.user.jid.split`@`[0]}\nJoin: ${timestamp}\n`, m);
-      global.tryConnect[numJadibot] = 0;
-      global.conns[numJadibot] = bajing;
+      global.conns.push(bajing)
+      bajing.reply(m.chat, `Berhasil Tersambung dengan WhatsApp mu.\n*NOTE: Cuma Numpang!*\nNomor: ${bajing.user.jid.split`@`[0]}\nJoin: ${timestamp}\n`, m)
+      global.tryConnect[numJadibot] = 0
+      global.conns[numJadibot] = bajing
     } else if (connection === 'close') {
-      m.reply('koneksi terputus!! wait...');
+      m.reply('koneksi terputus!! wait...')
     } else {
-      m.reply('Report Owner! BugError: ' + lastDisconnect.error.output);
+      m.reply('Report Owner! BugError: ' + lastDisconnect.error.output)
     }
   }
 
   global.tryConnect = function tryConnect(restatConn, close) {
-    bajing.welcome = 'Hai, @user!\nSelamat datang di grup @subject\n\n@desc';
-    bajing.bye = 'Selamat tinggal @user!';
-    bajing.spromote = '@user sekarang admin!';
-    bajing.sdemote = '@user sekarang bukan admin!';
-    bajing.handler = handler.bind(bajing);
-    bajing.connectionUpdate = needUpdate.bind(bajing);
-    bajing.credsUpdate = saveCreds.bind(conn);
+    bajing.welcome = 'Hai, @user!\nSelamat datang di grup @subject\n\n@desc'
+    bajing.bye = 'Selamat tinggal @user!'
+    bajing.spromote = '@user sekarang admin!'
+    bajing.sdemote = '@user sekarang bukan admin!'
+    bajing.handler = handler.bind(bajing)
+    bajing.connectionUpdate = needUpdate.bind(bajing)
+    bajing.credsUpdate = saveCreds.bind(conn)
 
     if (restatConn) {
       try {
-        bajing.ws.close();
+        bajing.ws.close()
       } catch {}
       conn = {
         ...bajing
-      };
+      }
     }
 
     if (!isInit || !close) {
-      ev.off('messages.upsert', bajing.handler);
+      ev.off('messages.upsert', bajing.handler)
 
-      ev.off('connection.update', bajing.connectionUpdate);
-      ev.off('creds.update', bajing.credsUpdate);
+      ev.off('connection.update', bajing.connectionUpdate)
+      ev.off('creds.update', bajing.credsUpdate)
     }
-    ev.on('messages.upsert', bajing.handler);
-    ev.on('connection.update', bajing.connectionUpdate);
-    ev.on('creds.update', bajing.credsUpdate);
+    ev.on('messages.upsert', bajing.handler)
+    ev.on('connection.update', bajing.connectionUpdate)
+    ev.on('creds.update', bajing.credsUpdate)
 
-    isInit = false;
-    return true;
+    isInit = false
+    return true
   }
 
-  await global.tryConnect();
+  await global.tryConnect()
 }
 
-handler.help = ['pairing'];
-handler.tags = ['jadibot', 'baileys'];
-handler.command = /^(pairing)$/i;
-handler.private = true;
-export default handler;
+handler.help = ['pairing']
+handler.tags = ['jadibot', 'baileys']
+handler.command = /^(pairing)$/i
+handler.private = true
+export default handler
 
-const more = String.fromCharCode(8206);
-const readMore = more.repeat(4001);
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
