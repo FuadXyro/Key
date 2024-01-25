aea2c82a69a2e16c47796bf83144311b6c11e4fa
FuadXyro <fuadxy99@gmail.com>
2024-01-25 17:14:53 +0100

    fix

--- plugins/adv-update.js
+++ plugins/adv-update.js
@@ -2,7 +2,7 @@ import cp from 'child_process'
 import { promisify, format } from 'util'
 let exec = promisify(cp.exec).bind(cp)
 let handler = async (m, { conn, isOwner }) => {
-    let caption = `"ZenithBotz"`
+    let caption = `"fix"`
     if (!isOwner) return
 	await conn.reply(m.chat, "Mohon tunggu...\nSedang mengupload script ke github", fkontak)
     exec(`git config --global user.email "fuadxy99@gmail.com" && git config --global user.name "FuadXyro" && git add . && git commit -m ${caption} && git push`, (stdout, stderr) => {
