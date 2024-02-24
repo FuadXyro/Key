64bb28e3206a28703b648b94182b16099790e940
FuadXyro <fuadxy99@gmail.com>
2024-02-24 17:12:07 +0700

    Key

--- plugins/_ownOnly.js
+++
@@ -1,8 +0,0 @@
-/* Buatan FuadXyro */
-export async function before(m, { isOwner }) {
-  let chat = global.db.data.chats[m.chat]
-  if (!('owneronly' in chat)) chat.owneronly = false 
-  if (chat.owneronly && !isOwner) {
-    throw new Error("Hanya pemilik yang diizinkan akses.")
-  }
-}
